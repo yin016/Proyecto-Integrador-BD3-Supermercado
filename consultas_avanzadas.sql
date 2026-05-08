de7fac9da903960206dd3ebe702de4b257b1f453
Daniel Ezequiel Zarate <109706052+yin016@users.noreply.github.com>
2026-05-08 19:23:12 -0300

    Delete consultas_avanzadas.sql

--- consultas_avanzadas.sql
+++
@@ -1,9 +0,0 @@
--- 1. CTE Recursiva: Ver categorías como un árbol [cite: 30]
-WITH RECURSIVE Arbol AS (
-    SELECT id, nombre, parent_id, 1 as nivel FROM categorias WHERE parent_id IS NULL
-    UNION ALL
-    SELECT c.id, c.nombre, c.parent_id, a.nivel + 1 FROM categorias c JOIN Arbol a ON c.parent_id = a.id
-) SELECT * FROM Arbol;
-
--- 2. Window Function: Ranking de productos más caros [cite: 29]
-SELECT nombre, precio, RANK() OVER (ORDER BY precio DESC) as ranking FROM productos LIMIT 10;
